d046fdbb5e398c868ff1be95221d3210df2053dc
mogmog17 <108569805+mogmog17@users.noreply.github.com>
2022-09-04 14:31:22 +0000

    [Add]いいね機能非同期通信完成

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -3,13 +3,11 @@ class Public::FavoritesController < ApplicationController
     @post = Post.find(params[:post_id])
     favorite = current_user.favorites.new(post_id: @post.id)
     favorite.save
-    redirect_to posts_path
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     favorite = current_user.favorites.find_by(post_id: @post.id)
     favorite.destroy
-    redirect_to posts_path
   end
 end
--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -30,6 +30,7 @@ class Public::PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
+    redirect_to post_path(@post.id)
   end
 
   def destroy
