a8ed7311e437a522243a47d36dadb176969e2c7d
mogmog17 <108569805+mogmog17@users.noreply.github.com>
2022-09-12 05:36:58 +0000

    [Add]管理者機能の追加

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,10 +1,21 @@
 class Admin::PostsController < ApplicationController
   def index
+    @posts = Post.page(params[:id])
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to admin_posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :spot_image, :star)
   end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,21 @@
 class Admin::UsersController < ApplicationController
   def index
+    @users = User.page(params[:page]).per(10)
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to admin_users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :introduction, :profile_image)
   end
 end
