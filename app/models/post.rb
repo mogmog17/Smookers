7a24fd515d781c1d2f9c287491c2c4b22b3f30f1
mogmog17 <108569805+mogmog17@users.noreply.github.com>
2022-09-15 12:53:24 +0000

    [Fix]地図表示のコード修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,7 +14,7 @@ class Post < ApplicationRecord
 
   def get_spot_image(width, height)
     unless spot_image.attached?
-      file_path = Rails.root.join('app/assets/images/NoImage.jpeg')
+      file_path = Rails.root.join('app/assets/images/NoImage.jpg')
       spot_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     spot_image.variant(resize_to_limit: [width, height]).processed
