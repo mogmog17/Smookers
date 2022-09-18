ee89932c6916e02ca61aba242be737f49fac7883
mogmog17 <108569805+mogmog17@users.noreply.github.com>
2022-09-18 09:06:48 +0000

    [Add]スライドショーの追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,13 @@ import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+
 import "jquery";
 import "popper.js";
+//import "skipper-slider/dist/skippr.js"
+//import "script.js"
+
+
 import "bootstrap";
 import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all'
